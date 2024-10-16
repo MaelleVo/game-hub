7584a37f63c49519abcb4f711eadcca19aec9335
MaelleVo <maellevolz@gmail.com>
2024-10-16 10:59:00 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import ListGroup from "./components/ListGroup";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+
+function App() {
+  let items = ["New York", "Tokyo", "Paris", "Singapour", "London"];
+  return <></>;
+}
+
+export default App;
